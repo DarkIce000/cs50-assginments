855cc781e766867073694d4d52056b3f004ce12f
aarav7397 <aarav7397@users.noreply.github.com>
2023-05-20 07:33:25 +0530

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("height: ");
+    }
+    while( n<1 ||n>9 );
+
+
+    //implementation of pattern
+
+
+//for column printing
+    int i;
+    for ( i = 0; i<n; i++)
+    {
+            //for rows printing
+        int k;
+        for (k=(n-i); k>1 ; k-- )
+            {
+                printf(" ");
+            }
+
+            //for row printing
+        int j;
+        for (j=0; j < i; j++ )
+
+           {
+            printf("#");
+           }
+           //for priting new line after every row printed
+
+        printf("\n");
+
+    }
+
+}
