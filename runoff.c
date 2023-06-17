c09994d01581d56cd25fcc71af4cbb9efc2034fd
aarav7397 <aarav7397@users.noreply.github.com>
2023-06-17 11:57:57 +0530

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -219,14 +219,17 @@ bool is_tie(int min)
     bool tie = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min)
+        if(!candidates[i].eliminated)
         {
-            tie = true;
-        }
-        else;
-        {
-            tie = false;
-            break;
+            if (candidates[i].votes == min)
+            {
+                tie = true;
+            }
+            else;
+            {
+                tie = false;
+                break;
+            }
         }
     }
     return tie;
