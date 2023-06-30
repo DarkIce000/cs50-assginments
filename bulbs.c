c92bc0636bf0d787f0ed5369fcb821276eedf02f
DarkIce000 <DarkIce000@users.noreply.github.com>
2023-06-30 21:14:30 +0530

    automated commit by submit50

--- bulbs.c
+++ bulbs.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int BITS_IN_BYTE = 8;
 
@@ -9,31 +9,31 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    //getting the string
+    // getting the string
     string input = get_string("input: ");
 
-    //convert into integer
+    // convert into integer
     int array[strlen(input)];
-    for(int i = 0; i < strlen(input); i++)
+    for (int i = 0; i < strlen(input); i++)
     {
         array[i] = input[i];
     }
 
-    for(int i = 0; i < strlen(input); i++)
+    for (int i = 0; i < strlen(input); i++)
     {
-        //putting the array into another array
+        // putting the array into another array
         int bit[8];
-        for(int j = BITS_IN_BYTE - 1; j >= 0; j--)
+        for (int j = BITS_IN_BYTE - 1; j >= 0; j--)
         {
-            //putting remainder in bit array
-            if(array[i]%2 == 1)
+            // putting remainder in bit array
+            if (array[i] % 2 == 1)
                 bit[j] = 1;
             else
                 bit[j] = 0;
-            array[i]/=2;
+            array[i] /= 2;
         }
 
-        //for printing in right to left direction
+        // for printing in right to left direction
         for (int j = 0; j < 8; j++)
         {
             print_bulb(bit[j]);
@@ -41,7 +41,6 @@ int main(void)
         printf("\n");
     }
 }
-
 
 void print_bulb(int bit)
 {
