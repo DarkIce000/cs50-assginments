6dd2e51b1ea4bee6d7d87171ee55043fd81ee0c5
DarkIce000 <DarkIce000@users.noreply.github.com>
2023-07-26 19:03:28 +0530

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars Where person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper" AND "Jennifer Lawrence"));
+SELECT title FROM movies WHERE id IN(SELECT movie_id FROM stars Where person_id IN(SELECT id FROM people WHERE name = "Bradley Cooper" or name = "Jennifer Lawrence"));
