50f3ca21ea9ac2bb121b482a607ed01895a463ad
DarkIce000 <DarkIce000@users.noreply.github.com>
2023-08-15 05:16:41 +0000

    automated commit by submit50

--- app.py
+++ app.py
@@ -24,17 +24,20 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
+
 def username():
-        id  = session["user_id"]
-        username = db.execute("SELECT username FROM users WHERE id = ?", id)
-        if username is None:
-            return None
-        else:
-            return username[0]["username"]
+    id = session["user_id"]
+    username = db.execute("SELECT username FROM users WHERE id = ?", id)
+    if username is None:
+        return None
+    else:
+        return username[0]["username"]
+
 
 def transaction_time():
     time = datetime.datetime.now(pytz.timezone("US/Eastern"))
     return time
+
 
 @app.after_request
 def after_request(response):
@@ -48,26 +51,35 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-
-    #order details and shares of each bought companies, total cash
-    portfolio = db.execute("SELECT symbols, SUM(shares) AS total_shares FROM transactions WHERE user_id = ? GROUP BY symbols", username())
+    # order details and shares of each bought companies, total cash
+    portfolio = db.execute(
+        "SELECT symbols, SUM(shares) AS total_shares FROM transactions WHERE user_id = ? GROUP BY symbols",
+        username(),
+    )
     data_list = []
     stock_price = 0.00
     for items in portfolio:
-            look_up = lookup(items["symbols"])
-            total = look_up["price"] * items["total_shares"]
-            stock_price = stock_price + total
-            data_list.append(look_up)
+        look_up = lookup(items["symbols"])
+        total = look_up["price"] * items["total_shares"]
+        stock_price = stock_price + total
+        data_list.append(look_up)
     cash = db.execute("SELECT cash FROM users WHERE username = ?", username())
     total_amt = stock_price + cash[0]["cash"]
-    #price to be added
-    return render_template("index.html", portfolio = portfolio, data_list = data_list, cash = cash[0]["cash"], total = total_amt)
+    # price to be added
+    return render_template(
+        "index.html",
+        portfolio=portfolio,
+        data_list=data_list,
+        cash=cash[0]["cash"],
+        total=total_amt,
+    )
+
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     if request.method == "POST":
-        #getting neccessary data at once
+        # getting neccessary data at once
         try:
             name = request.form.get("symbol")
             share = int(request.form.get("shares"))
@@ -82,39 +94,53 @@ def buy():
         if data is None:
             return apology("data[symbol]")
 
-        available_cash = db.execute("SELECT cash FROM users WHERE username = ? ", username())
+        available_cash = db.execute(
+            "SELECT cash FROM users WHERE username = ? ", username()
+        )
 
         if not available_cash:
             return apology("no cash")
-        #checking for possible errors
+        # checking for possible errors
         if not username() == None:
-
             if share <= 0:
                 return apology("INVALID Symbol")
             if share * data["price"] > available_cash[0]["cash"]:
                 return apology("bhikhaari sala")
 
-            #store in sqlite table
-            db.execute("INSERT INTO transactions (user_id, transaction_type, symbols, shares, price, transaction_time) VALUES (?, ?, ?, ?, ?, ?)", username(), 'buy', data["name"], share, data["price"], transaction_time())
+            # store in sqlite table
+            db.execute(
+                "INSERT INTO transactions (user_id, transaction_type, symbols, shares, price, transaction_time) VALUES (?, ?, ?, ?, ?, ?)",
+                username(),
+                "buy",
+                data["name"],
+                share,
+                data["price"],
+                transaction_time(),
+            )
 
-            update_cash  = (available_cash[0]["cash"] - (share * data["price"]))
-            db.execute("UPDATE users SET cash = ? where username = ?", update_cash, username())
+            update_cash = available_cash[0]["cash"] - (share * data["price"])
+            db.execute(
+                "UPDATE users SET cash = ? where username = ?", update_cash, username()
+            )
 
-            #redirect "TO HOMEPAGE
+            # redirect "TO HOMEPAGE
             return redirect("/")
         else:
             return apology("error")
     else:
         return render_template("buy.html")
 
+
 @app.route("/history")
 @login_required
 def history():
-
     """Show history of transactions"""
-    data = db.execute("SELECT symbols, shares, price, transaction_time FROM transactions WHERE user_id = ? ORDER BY transaction_time DESC", username())
+    data = db.execute(
+        "SELECT symbols, shares, price, transaction_time FROM transactions WHERE user_id = ? ORDER BY transaction_time DESC",
+        username(),
+    )
 
-    return render_template("history.html", data = data)
+    return render_template("history.html", data=data)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -126,7 +152,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -136,10 +161,14 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -151,6 +180,7 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
@@ -167,13 +197,15 @@ def logout():
 @login_required
 def quote():
     if request.method == "POST":
-       name = request.form.get("symbol")
-       data = lookup(name)
-       if name is None:
-           return apology("invalid")
-       if data is None:
-           return apology("invalid")
-       return render_template("quoted.html", Name = data["name"], Symbol = data["symbol"] , Price = data["price"])
+        name = request.form.get("symbol")
+        data = lookup(name)
+        if name is None:
+            return apology("invalid")
+        if data is None:
+            return apology("invalid")
+        return render_template(
+            "quoted.html", Name=data["name"], Symbol=data["symbol"], Price=data["price"]
+        )
     else:
         return render_template("quote.html")
 
@@ -187,7 +219,7 @@ def register():
 
         check_usr = db.execute("SELECT username FROM users GROUP BY username")
 
-        if username is None or not  len(username) > 1 or not len(password) > 1:
+        if username is None or not len(username) > 1 or not len(password) > 1:
             return apology("Username/ Password Required!")
         if not password == confirmation:
             return apology("Password donot match")
@@ -203,6 +235,7 @@ def register():
     else:
         return render_template("register.html")
 
+
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
@@ -215,9 +248,17 @@ def sell():
             return apology("invalid value")
         transaction_type = "sell"
 
-    #for available balance
-        sShares = db.execute("SELECT SUM(shares) AS sumsShares FROM transactions where user_id = ? AND symbols = ? AND transaction_type = 'sell'", username(), symbol)
-        bShares = db.execute("SELECT SUM(shares) AS sumsShares FROM transactions where user_id = ? AND symbols = ? AND transaction_type = 'buy'", username(), symbol)
+        # for available balance
+        sShares = db.execute(
+            "SELECT SUM(shares) AS sumsShares FROM transactions where user_id = ? AND symbols = ? AND transaction_type = 'sell'",
+            username(),
+            symbol,
+        )
+        bShares = db.execute(
+            "SELECT SUM(shares) AS sumsShares FROM transactions where user_id = ? AND symbols = ? AND transaction_type = 'buy'",
+            username(),
+            symbol,
+        )
         look_up = lookup(symbol)
         sell_price = look_up["price"]
         if sShares[0]["sumsShares"] == None:
@@ -226,7 +267,7 @@ def sell():
             available_shares = bShares[0]["sumsShares"] - sShares[0]["sumsShares"]
 
         tTime = transaction_time()
-    #error handling
+        # error handling
         if sell_shares > available_shares:
             return apology("You don't have that much of shares")
         if symbol == None:
@@ -234,11 +275,26 @@ def sell():
         if sell_shares <= 0:
             return apology("Please choose valid number of shares")
 
-    #entering the selled share in db
-        db.execute("UPDATE users SET cash = cash + ? WHERE username = ?", sell_price * sell_shares, username())
-        db.execute("INSERT INTO transactions (user_id, transaction_type, symbols, shares, price, transaction_time) VALUES (?, ?, ?, ?, ?, ?)", username(), transaction_type, symbol, -sell_shares, sell_price, transaction_time())
+        # entering the selled share in db
+        db.execute(
+            "UPDATE users SET cash = cash + ? WHERE username = ?",
+            sell_price * sell_shares,
+            username(),
+        )
+        db.execute(
+            "INSERT INTO transactions (user_id, transaction_type, symbols, shares, price, transaction_time) VALUES (?, ?, ?, ?, ?, ?)",
+            username(),
+            transaction_type,
+            symbol,
+            -sell_shares,
+            sell_price,
+            transaction_time(),
+        )
         return redirect("/")
 
     else:
-        db_symbols = db.execute("SELECT symbols FROM transactions where user_id = ? GROUP BY symbols", username())
-        return render_template("sell.html", db_symbols = db_symbols)
+        db_symbols = db.execute(
+            "SELECT symbols FROM transactions where user_id = ? GROUP BY symbols",
+            username(),
+        )
+        return render_template("sell.html", db_symbols=db_symbols)
