004ab188dfb36f35ddacc2e399489f47ae7ec5ba
darkice000 <aarav7397@gmail.com>
2023-09-26 23:36:42 +0530

    after reset

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from markdown2 import Markdown
 
 from django.utils.safestring import mark_safe
 from . import util
@@ -10,14 +8,3 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-def title(request, name):
-    name = name.upper()
-    markdowner = Markdown()
-    convertedInHtml = markdowner.convert(util.get_entry(name))
-    if util.get_entry(name):
-        return render(request, "encyclopedia/title.html", {
-            "page": convertedInHtml
-        })
-
-    return HttpResponse(f"Page doesnot exist, you can create one")
---
+++ title/customFunctions.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from encyclopedia import util
+from markdown2 import Markdown
+
+
+def render_title(request, name):
+    markdowner = Markdown()
+    if util.get_entry(name):
+        convertedInHtml = markdowner.convert(util.get_entry(name))
+        return render(request, "title/title.html", {
+            "page": convertedInHtml
+        })
+
+
+def checker(name):
+    for title in util.list_entries():
+        if name.lower() == title.lower():
+            return title
+         
+    return None
