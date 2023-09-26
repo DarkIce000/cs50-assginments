b0a8479b245cf3e290bc41097119a842b82148fc
darkice000 <aarav7397@gmail.com>
2023-09-26 21:32:47 +0530

    here /title working not /wiki/title so implement in side branch

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
 
+from django.utils.safestring import mark_safe
 from . import util
 
 
@@ -8,3 +11,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, name):
+    name = name.upper()
+    markdowner = Markdown()
+    convertedInHtml = markdowner.convert(util.get_entry(name))
+    if util.get_entry(name):
+        return render(request, "encyclopedia/title.html", {
+            "page": convertedInHtml
+        })
+
+    return HttpResponse(f"Page doesnot exist, you can create one")
